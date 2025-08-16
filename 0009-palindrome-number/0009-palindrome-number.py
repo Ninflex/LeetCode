343e26009dc2c85e05ebb90b87e101fc7eaeb3fe
Kajus Gajauskas <145608156+Ninflex@users.noreply.github.com>
2025-08-16 05:41:10 +0100

    Time: 19 ms (20.66%), Space: 12.5 MB (10.84%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+
+        reverse = int(str(x)[::-1])
+        if reverse == x:
+            return True
+        else:
+            return False
+
+x = 121
+solution = Solution()
+print(solution.isPalindrome(x))

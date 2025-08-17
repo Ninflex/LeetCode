a34648ff41d2eb74ed96c62b28d0e4674fdad831
Kajus Gajauskas <145608156+Ninflex@users.noreply.github.com>
2025-08-17 23:07:48 +0100

    Time: 0 ms (100%), Space: 13 MB (90.36%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-        return []
+        lookup = {}
 
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in lookup:
+                return [lookup[complement],i]
+            lookup[num] = i
+        return []
 
 nums = [2,7,11,15]
 target = 9

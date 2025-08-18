cc33f1267ed202ccd560900ad9baa8db663b13a9
Kajus Gajauskas <145608156+Ninflex@users.noreply.github.com>
2025-08-18 21:20:30 +0100

    Time: 3 ms (6.05%), Space: 13.1 MB (22.27%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        for i in range(len(nums)):
+            if target == nums[i]:
+                return i
+            elif target < nums[i]:
+                return i
+        return i+1
+
+        
